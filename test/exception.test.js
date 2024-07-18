fdf4f5eb49529d511b35ac40bbe9209398579d14
chuluq <chuluq@arvis.id>
2024-07-18 09:27:12 +0700

    test: coverage

--- test/exception.test.js
+++ test/exception.test.js
@@ -5,3 +5,7 @@ test("exception", () => {
   expect(() => callMe("Chuluq")).toThrow(MyException);
   expect(() => callMe("Chuluq")).toThrow("Oops my exception happens");
 });
+
+test("exception not happens", () => {
+  expect(callMe("Budi")).toBe("OK");
+});
